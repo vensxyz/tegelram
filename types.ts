7af20d1a232d799e218ea42003d45a61c14f19dc
Werner Neethling <wernertn@gmail.com>
2020-04-20 05:22:24 -0400

    Added more types

--- types.ts
+++ types.ts
@@ -16,6 +16,21 @@ export interface ISetWebhookParams {
     max_connections?: number;
     allowed_updates?: string[];
 }
+export interface ISendMessageParams {
+    chat_id: number | string;
+    text: string;
+    parse_mode?: string;
+    disable_web_page_preview?: boolean;
+    disable_notification?: boolean;
+    reply_to_message_id?: number;
+    reply_markup?: IInlineKeyboardMarkup | IReplyKeyboardMarkup | IReplyKeyboardRemove | IForceReply;
+}
+export interface IForwardMessageParams {
+    chat_id: number | string;
+    from_chat_id: number | string;
+    disable_notification?: boolean;
+    message_id: number;
+}
 export interface IWebhookInfo {
     url: string;
     has_custom_certificate: boolean;
@@ -26,8 +41,35 @@ export interface IWebhookInfo {
     allowed_updates?: string[];
 }
 export interface IUpdate {
+    update_id: number;
+    message?: string;
+    edited_message?: IMessage;
+    channel_post?: IMessage;
+    edited_channel_post?: IMessage;
+    inline_query?: IInlineQuery;
+    chosen_inline_result?: IChosenInlineResult;
+    callback_query?: ICallbackQuery;
+    shipping_query?: IShippingQuery;
+    pre_checkout_query?: IPreCheckoutQuery;
+    poll?: IPoll;
+    poll_answer?: IPollAnswer;
+}
+export interface IChosenInlineResult {
 
 }
+export interface IShippingQuery {
+
+}
+export interface IPreCheckoutQuery {
+
+}
+export interface ISendPhotoParams {
+    
+}
+export interface ISendAudioParams {
+    
+}
+
 
 // available types
 export interface IUser {
@@ -163,3 +205,6 @@ export interface IMaskPosition {
 }
 
 // inline mode
+export interface IInlineQuery {
+
+}
